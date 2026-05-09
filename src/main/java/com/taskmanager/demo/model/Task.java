59a4db5867aa4ee608e57a1beafe5d3334a54215
Akash Kumar <mkakash501@gmail.com>
2026-05-09 14:02:09 +0530

    task entity added

---
+++ src/main/java/com/taskmanager/demo/model/Task.java
@@ -0,0 +1,30 @@
+package com.taskmanager.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status = TaskStatus.PENDING;
+
+    private LocalDate dueDate;
+
+}
